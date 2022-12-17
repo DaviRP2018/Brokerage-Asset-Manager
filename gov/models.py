88305cc9802ca486f9ae0845b362b8d8687a2d52
davi-debug-blip-69 <42500271+DaviRP2018@users.noreply.github.com>
2022-12-17 17:48:25 -0300

    Adicionado impostos

--- gov/models.py
+++ gov/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class IRBaseSettings(models.Model):
+    """Configurações do quanto é cobrado"""
+    fee = models.FloatField()
+    is_percentage = models.BooleanField()
+
+
+class IRDividendSettings(IRBaseSettings):
+    """Configurações para os proventos"""
+    fee_type = models.CharField(max_length=255, default="Provento")
+
+
+class IR(models.Model):
+    """Controle de impostos pagos ou não"""
+    history = models.ForeignKey("History", on_delete=models.PROTECT)
+
+    value = models.FloatField()
+    paid = models.BooleanField()
+
+    due_date = models.DateField()
