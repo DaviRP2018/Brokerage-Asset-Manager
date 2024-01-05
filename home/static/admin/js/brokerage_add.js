be3d8f6fc4adadfa93f7aa9199e904bc60eb6e96
Davi Rodriguez <davi.rodriguez@jala.university>
2024-01-05 17:50:21 -0300

    JS Deposit logic Total auto complete

--- home/static/admin/js/brokerage_add.js
+++ home/static/admin/js/brokerage_add.js
@@ -38,6 +38,30 @@ $(document).ready(function () {
   }
 
 
+  function appendNotes() {
+    // TODO: put a setting to modify link as user
+    $("div.field-for_purchase_exchange_sell.field-purchase_value").append(
+      '<a class="fst-italic" href="https://www.bcb.gov.br/estabilidadefinanceira/historicocotacoes" target="_blank" rel="noopener noreferrer">Pra compra, ver o valor do dólar no dia pelo site do banco central</a>'
+    );
+    $("div.field-for_sale_exchange_purchase.field-sell_value").append(
+      '<a class="fst-italic" href="https://www.bcb.gov.br/estabilidadefinanceira/historicocotacoes" target="_blank" rel="noopener noreferrer">Pra proventos, cotação deve ser o valor do último dia útil da primeira quinzena do mês anterior</a>'
+    );
+
+    $("div.field-profit").append(
+      '<span class="fst-italic">Lucros isentos e não tributáveis, se abaixo de R$35.000,00</span>'
+    );
+  }
+
+
+  function clearAll() {
+    $("div.field-asset input, div.field-quantity input, div.field-price input, div.field-fees input, div.field-total input, div.field-origin_in_national_currency input, div.field-origin_in_foreign_currency input, div.field-for_purchase_exchange_sell input, div.field-purchase_value input, div.field-for_sale_exchange_purchase input, div.field-sell_value input").val(null);
+  }
+
+  function stringfyFloatBR(num) {
+    return num.toLocaleString('pt-BR')
+  }
+
+
   $("#id_operation").change(function (e) {
     e.preventDefault();
 
@@ -45,8 +69,9 @@ $(document).ready(function () {
     let borderColor = null;
 
     hideAll();
+    clearAll();
 
-    $(".brokerage-dynamic-field h2").html(selectedValue);
+    $(".brokerage-dynamic-field > h2").html(selectedValue);
     $(".brokerage-dynamic-field").show();
     $(".brokerage-dynamic-field").css("border-style", "groove");
     switch (selectedValue) {
@@ -62,22 +87,44 @@ $(document).ready(function () {
         $(".brokerage-dynamic-field").hide();
         break;
     }
-    $(".brokerage-dynamic-field h2").css("background", borderColor);
+    $(".brokerage-dynamic-field > h2").css("background", borderColor);
     $(".brokerage-dynamic-field").css("border-color", borderColor);
   });
 
 
+  // ============================ Exchange auto complete START ============================
+  $("div.field-for_purchase_exchange_sell > input").change(function (e) {
+    e.preventDefault();
+    let value = parseFloat($(this).val());
+    let feesValue = parseFloat($("div.field-fees input").val());
+    let totalValue = parseFloat($("div.field-total input").val());
+    let result = ((value * totalValue) + feesValue).toFixed(2);
+    $("div.field-purchase_value > input").val(stringfyFloatBR(result));
+  });
+
+  $("div.field-for_sale_exchange_purchase > input").change(function (e) {
+    e.preventDefault();
+    let value = parseFloat($(this).val());
+    let feesValue = parseFloat($("div.field-fees input").val());
+    let totalValue = parseFloat($("div.field-total input").val());
+    let result = ((value * totalValue) + feesValue).toFixed(2);
+    $("div.field-sell_value > input").val(stringfyFloatBR(result));
+  });
+  // ============================ Exchange auto complete END ============================
+
+
+  $("div.field-total input").change(function (e) {
+    e.preventDefault();
+    let selectedValue = $("#id_operation").val();
+    // Deposit logic
+    if (selectedValue === "Deposit") {
+      let value = parseFloat($(this).val());
+      // Fill national valor
+      $("div.field-origin_in_national_currency > input").val(stringfyFloatBR(value));
+    }
+  });
+
+
   hideAll();
-  // TODO: put a setting to modify link as user
-  $("div.field-for_purchase_exchange_sell.field-purchase_value").append(
-    '<a class="fst-italic" href="https://www.bcb.gov.br/estabilidadefinanceira/historicocotacoes" target="_blank" rel="noopener noreferrer">Pra compra, ver o valor do dólar no dia pelo site do banco central</a>'
-  );
-  $("div.field-for_sale_exchange_purchase.field-sell_value").append(
-    '<a class="fst-italic" href="https://www.bcb.gov.br/estabilidadefinanceira/historicocotacoes" target="_blank" rel="noopener noreferrer">Pra proventos, cotação deve ser o valor do último dia útil da primeira quinzena do mês anterior</a>'
-  );
-
-  $("div.field-profit").append(
-    '<span class="fst-italic">Lucros isentos e não tributáveis, se abaixo de R$35.000,00</span>'
-  );
-
+  appendNotes();
 });
